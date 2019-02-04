38858e61ce497b9dfde6138d8f17a0c4994144ae
virtusacloud <virtusacloudai@gmail.com>
2019-02-04 15:08:37 +0530

    first commit

---
+++ RetriveRecordService/src/main/java/com/vpod/retriverecordservice/RetriveRecordServiceApplication.java
@@ -0,0 +1,24 @@
+package com.vpod.retriverecordservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableEurekaClient
+public class RetriveRecordServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetriveRecordServiceApplication.class, args);
+	}
+	
+	@RequestMapping
+	public String retriveRecord() {
+		return "Record Retrived Successfully";
+	}
+
+}
+
---
+++ StoreRecordService/src/test/java/com/vpod/storerecordservice/StoreRecordServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.vpod.storerecordservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StoreRecordServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
